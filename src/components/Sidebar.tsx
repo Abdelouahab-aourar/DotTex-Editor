470ae01347b0f50744a0ca5c96984f69e148cb6c
Abdelouahab_a <wahabaourar00@gmail.com>
2026-02-07 12:08:38 +0100

    created the window control buttons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,12 +27,12 @@ export const Sidebar = ({ explorerOpen, setExplorerOpen, selected, setSelected }
     return (
         <aside className="w-15 h-full py-3 bg-background border-r border-white flex flex-col space-y-3 items-center">
             {
-                navItems.map((item) => {
+                navItems.map((item, key) => {
                     const isSelected = selected === item.id;
                     const IconTag = (isSelected && explorerOpen) ? item.activeIcon : item.inactiveIcon;
 
                     return (
-                        <div className={`px-2 py-2 ${isSelected ? "border-l-2 border-l-blue-500" : "border-l-2 border-l-transparent"} border-r-2 border-r-transparent flex items-center justify-center`}>
+                        <div key={key} className={`px-2 py-2 ${isSelected ? "border-l-2 border-l-blue-500" : "border-l-2 border-l-transparent"} border-r-2 border-r-transparent flex items-center justify-center`}>
                             <IconTag
                                 key={item.id}
                                 size={32}

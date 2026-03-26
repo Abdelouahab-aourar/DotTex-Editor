22bb822ea614bc89e3d1b26e4035e4998dfd0bb0
Abdelouahab_a <wahabaourar00@gmail.com>
2026-03-26 17:39:09 +0100

    code refactor

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,35 +1,8 @@
-// use tauri::{AppHandle, Manager};
-// use tauri_plugin_shell::process::CommandEvent;
 mod db;
 use db::{init_db, add_to_history, get_history};
 mod latex;
 use latex::compile_latex;
-// #[tauri::command]
-// async fn start_texlab(app_handle: AppHandle) {
-    // let sidecar_command = app_handle
-    //     .shell()
-    //     .sidecar("texlab")
-    //     .expect("failed to setup sidecar: check tauri.conf.json");
 
-    // let (mut rx, mut _child) = sidecar_command
-    //     .spawn()
-    //     .expect("failed to spawn texlab");
-    // tauri::async_runtime::spawn(async move {
-    //     while let Some(event) = rx.recv().await {
-    //         match event {
-    //             CommandEvent::Stdout(line_bytes) => {
-    //                 let line = String::from_utf8_lossy(&line_bytes).to_string();
-    //                 let _ = app_handle.emit("lsp-data", line);
-    //             }
-    //             CommandEvent::Stderr(err_bytes) => {
-    //                 let err = String::from_utf8_lossy(&err_bytes).to_string();
-    //                 let _ = app_handle.emit("lsp-error", err);
-    //             }
-    //             _ => {}
-    //         }
-    //     }
-    // });
-// }
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
